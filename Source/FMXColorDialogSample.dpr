aef8f58caec544d526ed8a765ed3585815c559f0
Jim McKeeth <jim@mckeeth.org>
2024-06-28 23:06:41 +0200

    experimental

--- Source/FMXColorDialogSample.dpr
+++ Source/FMXColorDialogSample.dpr
@@ -6,12 +6,16 @@ uses
   ColorDialog in 'ColorDialog.pas' {frmColorDialog},
   cmykutil in 'cmykutil.pas',
   X11ColorData in 'X11ColorData.pas',
-  HTMLColorData in 'HTMLColorData.pas';
+  HTMLColorData in 'HTMLColorData.pas',
+  ColorSliderConverter in 'ColorSliderConverter.pas' {Form1},
+  Jim.FMX.TrackAndSpin in 'Components\Jim.FMX.TrackAndSpin.pas',
+  Jim.FMX.MoreTrackbars in 'Components\Jim.FMX.MoreTrackbars.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
   Application.CreateForm(TfrmColorDialog, frmColorDialog);
   Application.Run;
 end.
