bbdd08ca61b22eac48d645fa4f0f9e8c04d7cdfc
Jim McKeeth <jim@mckeeth.org>
2024-07-07 18:09:20 -0600

    cleaning up color data

---
+++ data/colordata.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS colors;
+CREATE TABLE colors(
+    hex NVARCHAR,
+    name NVARCHAR, 
+    red SMALLINT,
+    green SMALLINT,
+    blue SMALLINT,
+    family NVARCHAR,
+    link NVARCHAR,
+    source NVARCHAR,
+    ID NVARCHAR,
+    hue SINGLE,
+    Saturation SINGLE,
+    Luminance SINGLE,
+    Cyan SINGLE,
+    Magenta SINGLE,
+    Yellow SINGLE,
+    Key SINGLE);
